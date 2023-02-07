7a2c79136b7d66f6ab3fb32eb69aac721bebde37
Gilmore Moses <gilmoremoses@outlook.com>
2023-02-07 21:17:42 +0200

    create eader components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,5 @@
-import { BrowserRouter, as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-export default function Home()
-{
-return(
-    <Router>
-        <Routes>
-            <Route path="/" element= {<Home />} />
-        </Routes>
-    </Router>
-)   
-}  
+export default function Home() {
+    return (
+      <div>Home</div>
+    )
+  }
